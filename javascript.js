94c5721261f7672e5b8d8f47ac8492ec209b1fee
code-me-live <patricebelda@gmail.com>
2024-03-03 21:53:08 +0100

    ajout descrition et image

--- javascript.js
+++ javascript.js
@@ -1,6 +1,26 @@
 var test = document.getElementById('list');
 console.log(test);
 
-var li = document.createElement('li')
-li.textContent = "Bonjour";
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+image1.src = "img/lapin.jpeg"
+li.textContent = "Article 1";
+text.textContent = "Description 1";
 test.appendChild(li);
+test.appendChild(text);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+li.textContent = "Article 2";
+text.textContent = "Description 2";
+test.appendChild(li);
+test.appendChild(text);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+li.textContent = "Article 3";
+text.textContent = "Description 3";
+test.appendChild(li);
+test.appendChild(text);
