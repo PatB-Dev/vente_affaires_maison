7610ffdc6e8df77d3fe421ffbf53820ea2a07774
code-me-live <patricebelda@gmail.com>
2024-03-04 12:22:06 +0100

    ajout articles

--- javascript.js
+++ javascript.js
@@ -4,23 +4,27 @@ console.log(test);
 var li = document.createElement('li');
 var text = document.createElement('p');
 var image1 = document.createElement('img');
-image1.src = "img/lapin.jpeg"
+var prix1 = document.createElement('b');
+image1.src = "img/Blender.jpg";
+image1.style.width = '200px'
+prix1.innerText = '(15€)'
 li.textContent = "Article 1";
-text.textContent = "Description 1";
+text.textContent = "Blender SylverCrest ";
 test.appendChild(li);
 test.appendChild(text);
+text.appendChild(prix1);
 test.appendChild(image1);
 
 var li = document.createElement('li');
 var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/Tableau.jpg";
+image1.style.width = '200px'
+prix1.innerText = '(Gratuit)'
 li.textContent = "Article 2";
-text.textContent = "Description 2";
+text.textContent = "Tableau blanc avec coté noir + 2 dictionnaires ";
 test.appendChild(li);
 test.appendChild(text);
-
-var li = document.createElement('li');
-var text = document.createElement('p');
-li.textContent = "Article 3";
-text.textContent = "Description 3";
-test.appendChild(li);
-test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
