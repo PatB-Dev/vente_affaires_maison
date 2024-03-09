81cbce04f25a2708956c4c031c944e293fcc116a
code-me-live <patricebelda@gmail.com>
2024-03-09 10:59:45 +0100

    ajout articles

--- javascript.js
+++ javascript.js
@@ -1,19 +1,6 @@
 var test = document.getElementById('list');
 console.log(test);
 
-var li = document.createElement('li');
-var text = document.createElement('p');
-var image1 = document.createElement('img');
-var prix1 = document.createElement('b');
-image1.src = "img/Blender.jpg";
-image1.style.width = '300px'
-prix1.innerText = '(15€)'
-li.textContent = "Article 1";
-text.textContent = "Blender SylverCrest ";
-test.appendChild(li);
-test.appendChild(text);
-text.appendChild(prix1);
-test.appendChild(image1);
 
 var li = document.createElement('li');
 var text = document.createElement('p');
@@ -33,11 +20,137 @@ var li = document.createElement('li');
 var text = document.createElement('p');
 var image1 = document.createElement('img');
 var prix1 = document.createElement('b');
+image1.src = "img/Blender.jpg";
+image1.style.width = '300px'
+prix1.innerText = '(15€)'
+li.textContent = "Article 1";
+text.textContent = "Blender SylverCrest ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/veloST50.jpg";
+image1.style.width = '600px'
+prix1.innerText = '150€'
+li.textContent = "Article 4";
+text.textContent = "2 Vélos ST50 (un avec panier sur le guidon) ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/veloST100.jpg";
+image1.style.width = '600px'
+prix1.innerText = '180€'
+li.textContent = "Article 5";
+text.textContent = "Vélo ST50 ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
 image1.src = "img/kit_ciseaux.jpg";
 image1.style.width = '300px'
 prix1.innerText = '150€'
 li.textContent = "Article 3";
 text.textContent = "Kit ciseaux Exthand ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/blouses_accessoires.jpg";
+image1.style.width = '600px'
+prix1.innerText = '50€'
+li.textContent = "Article 3";
+text.textContent = "2 blouses + accessoires ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/etau_tablier.jpg";
+image1.style.width = '600px'
+prix1.innerText = '50€'
+li.textContent = "Article 3";
+text.textContent = "Etau + tablier de coiffure ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/gabriel-M.jpg";
+image1.style.width = '500px'
+prix1.innerText = '20€/tête (à négocier)'
+li.textContent = "Article 6";
+text.textContent = "Tête de coiffure GABRIEL-M (X2) ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/kathie.jpg";
+image1.style.width = '500px'
+prix1.innerText = '20€ (à négocier)'
+li.textContent = "Article 7";
+text.textContent = "Tête de coiffure KATHIE ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/lizbeth-M.jpg";
+image1.style.width = '500px'
+prix1.innerText = '20€ (à négocier)'
+li.textContent = "Article 8";
+text.textContent = "Tête de coiffure LIZBETH-M ";
+test.appendChild(li);
+test.appendChild(text);
+text.appendChild(prix1);
+test.appendChild(image1);
+
+var li = document.createElement('li');
+var text = document.createElement('p');
+var image1 = document.createElement('img');
+var prix1 = document.createElement('b');
+image1.src = "img/lodie-M.jpg";
+image1.style.width = '500px'
+prix1.innerText = '20€ (à négocier)'
+li.textContent = "Article 9";
+text.textContent = "Tête de coiffure LODIE-M ";
 test.appendChild(li);
 test.appendChild(text);
 text.appendChild(prix1);
